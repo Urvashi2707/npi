0c2caba656f42e8d49471c15d9631aa56da1e70f
Urvashi Urvi <urvi@21north.in>
2018-07-19 14:50:17 +0530

    UI Changes

--- src/app/pages/reports/reports.component.ts
+++ src/app/pages/reports/reports.component.ts
@@ -4,8 +4,8 @@ import { Router } from '@angular/router';
 import { DatePipe } from '@angular/common';
 import { QueueTableService } from '../services/queue-table.service';
 import { NgxSpinnerService } from 'ngx-spinner';
-import { HttpClient, HttpHeaders, HttpErrorResponse, HttpRequest } from '@angular/common/http';
-import { NgbDateAdapter, NgbDateStruct, NgbDatepickerConfig, NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClient} from '@angular/common/http';
+import { NgbDateStruct, NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-reports',
   templateUrl: './reports.component.html',
@@ -20,27 +20,26 @@ export class ReportsComponent implements OnInit {
   reverse: boolean = false;
   model: NgbDateStruct;
   model1: NgbDateStruct;
-  dateString: string;
-  dateString1: string;
-  message: string;
-  p:number = 1;
+  EndDateString: string;
+  StrtDateString: string;
+  MessageNoData: string;
+  page:number = 1;
   user: any = {};
   InsuranceUsr:string;
   InsuranceCheck:boolean = false;
-  mydate: string;
-  pastdate:string;
-  today:string;
-  date1:string;
-  date2:string;
-  dataperpage:string;
-  record_count:string;
-
+  DataPerPage:string;
+  RecordCount:string;
   disableCust:boolean = false;
-  public globalsvcid:string;
-  public selectedsvcid:string;
-  public svcid:string
-  constructor(private spinner: NgxSpinnerService,private _detailsTable: QueueTableService,private datePipe: DatePipe, private router: Router, private ngbDateParserFormatter: NgbDateParserFormatter, private service: ServicingService, private http: HttpClient) {
-  }
+  public SvcID:string;
+
+  constructor(private spinner: NgxSpinnerService,
+              private _detailsTable: QueueTableService,
+              private datePipe: DatePipe, 
+              private router: Router, 
+              private ngbDateParserFormatter: NgbDateParserFormatter,
+              private service: ServicingService, 
+              private http: HttpClient) {
+            }
 
   ngOnInit() {
     this.InsuranceUsr = JSON.parse(sessionStorage.getItem('insurance'));
@@ -48,18 +47,16 @@ export class ReportsComponent implements OnInit {
     this.user.status = "1";
     var date = new Date();
     this.model = {day:date.getUTCDate(),month:date.getUTCMonth() + 1,year: date.getUTCFullYear() };
-    this.dateString = this.model.year + '-' + this.model.month + '-' + this.model.day;
-    this.today = this.datePipe.transform(date,"yyyy-MM-dd");
+    this.EndDateString = this.model.year + '-' + this.model.month + '-' + this.model.day;
     var dt = new Date();
            dt.setDate( dt.getDate() - 14 );
       this.model1 = { day: dt.getUTCDate(), month: dt.getUTCMonth() + 1, year: dt.getUTCFullYear()};
-      this.dateString1 = this.model1.year + '-' + this.model1.month + '-' + this.model1.day;
-      console.log(this.dateString1);
+      this.StrtDateString = this.model1.year + '-' + this.model1.month + '-' + this.model1.day;
     if(sessionStorage.getItem('selectedsvc')){
-      this.svcid = sessionStorage.getItem('selectedsvc');
+      this.SvcID = sessionStorage.getItem('selectedsvc');
     }
     else{
-      this.svcid = JSON.parse(sessionStorage.getItem('globalsvcid'));
+      this.SvcID = JSON.parse(sessionStorage.getItem('globalsvcid'));
     }
     if(this.InsuranceUsr == "1"){
       this.InsuranceCheck = true;
@@ -84,79 +81,58 @@ export class ReportsComponent implements OnInit {
 
     ];
     this.user.status = this.status[0].id;
-    this.search(1);
+    this.Search(1);
   }
 
-  onSelectDate(date: NgbDateStruct) {
+  onEndSelectDate(date: NgbDateStruct) {
     if (date != null) {
       this.model = date;
-      this.dateString = this.ngbDateParserFormatter.format(date);
-      console.log(this.dateString);
+      this.EndDateString = this.ngbDateParserFormatter.format(date);
     }
+}
 
-
-  }
-
-  onSelectDate1(date: NgbDateStruct) {
+  onStrtSelectDate(date: NgbDateStruct) {
     if (date != null) {
       this.model1 = date;
-      this.dateString1 = this.ngbDateParserFormatter.format(date);
-      console.log(this.dateString1);
+      this.StrtDateString = this.ngbDateParserFormatter.format(date);
     }
+}
 
-
-  }
   sort(key){
     this.key = key;
     this.reverse = !this.reverse;
   }
+
   setDefaultDate(): NgbDateStruct {
     var startDate = new Date();
     let startYear = startDate.getFullYear().toString();
     let startMonth = startDate.getMonth() + 1;
     let startDay = "1";
-
-    return this.ngbDateParserFormatter.parse(startYear + "-" + startMonth.toString() + "-" + startDay);
+  return this.ngbDateParserFormatter.parse(startYear + "-" + startMonth.toString() + "-" + startDay);
   }
-  search(p:number) {  
-    this.spinner.show();
-    this.p = p - 1 ;
 
+
+  Search(p:number) {  
+    this.spinner.show();
+    this.page = p - 1 ;
     if(this.user.service_type == "4" || this.user.service_type == "6"){
       this.disableCust = true;
     }
     else{
       this.disableCust = false;
     }
-    this.message = "";
-
-    if(this.dateString){
-      
-      this.date2 = this.dateString
-    }
-    else {
-      
-      this.date2 = this.today;
-    }
-    if(this.dateString1){
-      this.date1 = this.dateString1;
-    }
-    else{
-      this.date1 = this.pastdate;
-    }
-    const reqpara1 =
-      {
+    this.MessageNoData = null ;
+    const reportPara ={
         requesttype: 'getreportsv2',
-        startdate: this.dateString1,
-        enddate: this.dateString,
+        startdate: this.StrtDateString,
+        enddate: this.EndDateString,
         servicetype: this.user.service_type,
         status: this.user.status,
-        svcid:this.svcid,
-        pagenumber:this.p
-
-      }
-    const as1 = JSON.stringify(reqpara1)
-    this.service.webServiceCall(as1).subscribe
+        svcid:this.SvcID,
+        pagenumber:this.page
+     }
+    const reqpara = JSON.stringify(reportPara)
+    this.service.webServiceCall(reqpara).subscribe
       (res => {
         if (res[0].login === 0) {
           sessionStorage.removeItem('currentUser');
@@ -164,22 +140,16 @@ export class ReportsComponent implements OnInit {
         }
         else {
           if (res[0].pagecount[0].hasOwnProperty('noqueues')) {
-            this.message = "No Data";
-            console.log('No Data');
+            this.MessageNoData = "No Data";
             this.spinner.hide();
           }
           else {
-            this.record_count = res[0].pagecount[0].record_count;
-            this.dataperpage = res[0].pagecount[0].pagelimit;
+            this.RecordCount = res[0].pagecount[0].record_count;
+            this.DataPerPage = res[0].pagecount[0].pagelimit;
             this.report = res[1].record;
-            console.log(this.report);
             this.spinner.hide();
-          }
-
-          }
-
+          }}
         });
-
       }
   
       openQDetails(data:any){
@@ -189,7 +159,4 @@ export class ReportsComponent implements OnInit {
         this._detailsTable.queueID = data.id;
         this.router.navigate(['/pages/queue-details']);
       }
-
- 
-
 }
