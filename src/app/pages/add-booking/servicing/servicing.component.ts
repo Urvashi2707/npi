da68caffe9f396740383e18ba287e5cf9e61a73f
Urvashi Urvi <urvi@21north.in>
2018-08-07 23:50:14 +0530

    NPI-190 fixed

--- src/app/pages/add-booking/servicing/servicing.component.ts
+++ src/app/pages/add-booking/servicing/servicing.component.ts
@@ -875,27 +875,26 @@ public getCity() {
                   this.user.address_typedu = null;
                 }
           }
-          this.carinfo = this.customer[4].carinfo[0];
-          console.log( this.carinfo);
-          if(this.carinfo.hasOwnProperty('no_records')){
-            this.selectedBrand  = sessionStorage.getItem('brandid');
-             console.log(this.selectedBrand )
-             this.getModelds(this.selectedBrand );
+      }
+      this.carinfo = this.customer[4].carinfo[0];
+      console.log( this.carinfo);
+      if(this.carinfo.hasOwnProperty('no_records')){
+        this.selectedBrand  = sessionStorage.getItem('brandid');
+         console.log(this.selectedBrand )
+         this.getModelds(this.selectedBrand );
+      }
+      else{
+        this.selectedBrand = this.carinfo.brand_id;
+        this.getModelds(this.selectedBrand);
+        this.selectedModel = this.carinfo.veh_model_id;
+        this.selectedVariant = this.carinfo.veh_submodel_id;
+        this.getVariants(this.selectedModel);
+        for (let i = 0; i < this.Models.length; i++) {
+          if (this.Models[i].id === this.selectedModel) {
+            this.selectedModel = this.Models[i].id ;
           }
-          else{
-            this.selectedBrand = this.carinfo.brand_id;
-            this.getModelds(this.selectedBrand);
-            this.selectedModel = this.carinfo.veh_model_id;
-            this.selectedVariant = this.carinfo.veh_submodel_id;
-            this.getVariants(this.selectedModel);
-            for (let i = 0; i < this.Models.length; i++) {
-              if (this.Models[i].id === this.selectedModel) {
-                this.selectedModel = this.Models[i].id ;
-              }
-            }
-          }
-        
         }
+      }
       }},
       (err: HttpErrorResponse) => {
         if (err.error instanceof Error) {
