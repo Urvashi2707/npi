16a2815a3e2694bd3d3242c300e506e1ab0ed6bf
Urvashi Urvi <urvi@21north.in>
2018-10-05 19:23:49 +0530

    NPI - 230 and NPI - 225 fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,9 +5,9 @@
  */
 export const environment = {
   production: true,
-  Mainurl:'https://m.21north.in/notify/svcwebservice.php',
-  logout_url:"https://m.21north.in/notify/logout.php",
-  graph:"https://m.21north.in/notify/svcgraph.php",
-  slot_graph:"https://m.21north.in/notify/svcgraphslot.php",
-  upload_file:"https://m.21north.in/notify/uploadfile.php",
+  Mainurl:"/notify/svcwebservice.php",
+  logout_url:"/notify/logout.php'",
+  graph:"/notify/svcgraph.php",
+  slot_graph:"/notify/svcgraphslot.php",
+  upload_file:"/notify/uploadfile.php",
 };
